568d675f815da5483788e7db40556e544ef2fe90
Gavin Bauman <gavination@users.noreply.github.com>
2018-05-17 17:18:09 -0400

    Add ESLint. Adopted standards (#7)

--- parsers.js
+++ parsers.js
@@ -1,30 +1,30 @@
 const validate = require('./validators')
 
 module.exports = {
-    accessTokenToCredentials: (req) => {
-        // ensure we have a valid accessToken before proceeding
-        validate.accessToken(req)
+  accessTokenToCredentials: (req) => {
+    // ensure we have a valid accessToken before proceeding
+    validate.accessToken(req)
 
-        // this implements the contract defined by msRestAzure
-        // see https://github.com/Azure/azure-sdk-for-node/blob/0a52678ea7b3a24a478975f7169fc30e9fc9759e/runtime/ms-rest-azure/lib/credentials/deviceTokenCredentials.js
-        return {
-            signRequest: (webResource, callback) => {
-                webResource.headers['Authorization'] = req.body.accessToken
-                callback(null)
-            }
-        }
-    },
-    resourcesToObjects: (req) => {
-        // ensure we have a valid resources array before proceeding
-        validate.resources(req)
-
-        // parse the resources into objects
-        return req.body.resources.map(r => r.split('/')).map(r => {
-            return {
-                subscriptionId: r[0],
-                resourceGroupName: r[1],
-                resourceName: r[2]
-            }
-        })
+    // this implements the contract defined by msRestAzure
+    // see https://github.com/Azure/azure-sdk-for-node/blob/0a52678ea7b3a24a478975f7169fc30e9fc9759e/runtime/ms-rest-azure/lib/credentials/deviceTokenCredentials.js
+    return {
+      signRequest: (webResource, callback) => {
+        webResource.headers['Authorization'] = req.body.accessToken
+        callback(null)
+      }
     }
+  },
+  resourcesToObjects: (req) => {
+    // ensure we have a valid resources array before proceeding
+    validate.resources(req)
+
+    // parse the resources into objects
+    return req.body.resources.map(r => r.split('/')).map(r => {
+      return {
+        subscriptionId: r[0],
+        resourceGroupName: r[1],
+        resourceName: r[2]
+      }
+    })
+  }
 }
--- test/basic.js
+++ test/basic.js
@@ -1,98 +1,100 @@
 const assert = require('assert')
 const index = require('../')
 
+/* eslint-env node, mocha */
+
 describe('platform-chaos', () => {
-    it('is named properly', () => {
-        assert.equal(require('../package.json').name, 'platform-chaos')
-        assert.equal(require('../package-lock.json').name, 'platform-chaos')
-    })
-    it('parses resources', () => {
-        assert.throws(() => {
-            index.validators.resources({
-                body: {
-                    resources: [1]
-                }
-            })
-        })
-
-        assert.throws(() => {
-            index.validators.resources({
-                body: {
-                    resources: ['']
-                }
-            })
-        })
-
-        assert.throws(() => {
-            index.validators.resources({
-                body: {
-                    resources: ['one/two/three/four']
-                }
-            })
-        })
-
-        const instance = index.parsers.resourcesToObjects({
-            body: {
-                resources: ['sub/rg/resource']
-            }
-        })[0]
-
-        assert.equal(instance.subscriptionId, "sub")
-        assert.equal(instance.resourceGroupName, "rg")
-        assert.equal(instance.resourceName, "resource")
-    })
-
-    it('parses accessTokens', () => {
-        // invalid at
-        assert.throws(() => {
-            index.validators.accessToken({
-                body: {
-                    accessToken: 1
-                }
-            })
-        })
-
-        // empty resourceIds
-        assert.throws(() => {
-            index.validators.accessToken({
-                    body: {
-                    accessToken: "valid type"
-            }
-            })
-        })
-
-        // invalid resourceIds type
-        assert.throws(() => {
-            index.validators.accessToken({
-                body: {
-                    accessToken: "valid type"
-                }
-            })
-        })
-
-        // invalid resourceIds format
-        assert.throws(() => {
-            index.validators.accessToken({
-                body: {
-                    accessToken: "valid type"
-                }
-            })
-        })
-
-        // valid
-        const expectedAccessToken = "Bearer 12345234r2"
-        const instance =index.parsers.accessTokenToCredentials({
-            body: {
-                accessToken: expectedAccessToken
-            }
-        })
-
-        const res = {
-            headers: {}
+  it('is named properly', () => {
+    assert.equal(require('../package.json').name, 'platform-chaos')
+    assert.equal(require('../package-lock.json').name, 'platform-chaos')
+  })
+  it('parses resources', () => {
+    assert.throws(() => {
+      index.validators.resources({
+        body: {
+          resources: [1]
         }
-
-        instance.signRequest(res, () => {})
-
-        assert.equal(res.headers['Authorization'], expectedAccessToken)
+      })
     })
+
+    assert.throws(() => {
+      index.validators.resources({
+        body: {
+          resources: ['']
+        }
+      })
+    })
+
+    assert.throws(() => {
+      index.validators.resources({
+        body: {
+          resources: ['one/two/three/four']
+        }
+      })
+    })
+
+    const instance = index.parsers.resourcesToObjects({
+      body: {
+        resources: ['sub/rg/resource']
+      }
+    })[0]
+
+    assert.equal(instance.subscriptionId, 'sub')
+    assert.equal(instance.resourceGroupName, 'rg')
+    assert.equal(instance.resourceName, 'resource')
+  })
+
+  it('parses accessTokens', () => {
+    // invalid at
+    assert.throws(() => {
+      index.validators.accessToken({
+        body: {
+          accessToken: 1
+        }
+      })
+    })
+
+    // empty resourceIds
+    assert.throws(() => {
+      index.validators.accessToken({
+        body: {
+          accessToken: 'valid type'
+        }
+      })
+    })
+
+    // invalid resourceIds type
+    assert.throws(() => {
+      index.validators.accessToken({
+        body: {
+          accessToken: 'valid type'
+        }
+      })
+    })
+
+    // invalid resourceIds format
+    assert.throws(() => {
+      index.validators.accessToken({
+        body: {
+          accessToken: 'valid type'
+        }
+      })
+    })
+
+    // valid
+    const expectedAccessToken = 'Bearer 12345234r2'
+    const instance = index.parsers.accessTokenToCredentials({
+      body: {
+        accessToken: expectedAccessToken
+      }
+    })
+
+    const res = {
+      headers: {}
+    }
+
+    instance.signRequest(res, () => {})
+
+    assert.equal(res.headers['Authorization'], expectedAccessToken)
+  })
 })
